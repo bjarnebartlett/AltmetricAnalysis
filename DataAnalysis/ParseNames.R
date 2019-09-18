69ea772d2bfd7c693f6cc8e134853f04aae4224f
Julie <jfortin@uvic.ca>
2019-09-17 22:46:16 -0700

    ran 1000 bioRxiv articles at a time; encountered some issues, fixed by further cleaning

--- DataAnalysis/ParseNames.R
+++ DataAnalysis/ParseNames.R
@@ -123,8 +123,8 @@ afn.assessonejournal <- function(journal_data, outfile) {
   
   newjournaldata <- c()
   
-  # for (i in 1:dim(journal_data)[[1]]) { # go through all articles
-  for (i in 1:1000) {
+  for (i in 1:dim(journal_data)[[1]]) { # go through all articles
+  # for (i in 9001:10000) {
   # for (i in startarticlenum:endarticlenum) { { # for testing purposes to keep within 1000 api limit
     # pull data for just one article
     
@@ -235,6 +235,8 @@ afn.cleanauthors <- function(authorsvector) {
   
   authors <- gsub("\\\\xa0"," ",authorsvector)
   authors <- gsub("[^[:alnum:]^\'^-]"," ",authors)
+  authors <- gsub("[[:digit:]+]","",authors)
+  authors <- gsub("-[[:lower:]] ","- ",authors)
   
   authorsvec <- strsplit(authors,"\' ")[[1]]
   authorsvec <- gsub(" \'", "", authorsvec)
@@ -273,8 +275,7 @@ afn.cleanauthors <- function(authorsvector) {
       authorsvec[i] <- auth
     }
   }
-  authorsvec <- gsub(" [A-Z] "," ",authorsvec) # get rid of any lone initials (most likely middle initial)
-  
+  authorsvec <- gsub(" [A-z] "," ",authorsvec) # get rid of any lone initials (most likely middle initial)
   
   # authorsvec <- gsub("[A-Z] ","",authorsvec) # get rid of beginning/middle initials
   # authorsvec <- gsub(" [A-Z]$","",authorsvec) # get rid of initials at end
